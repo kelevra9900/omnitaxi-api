e7536087c4a7bb02fc05ffabd5c82cfa4d8da2d8
Roger Torres <torresroger445@gmail.com>
2026-02-27 21:31:08 -0600

    under construction flex flow

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,4 +1,6 @@
 export const jwtConstants = {
-  secret: process.env.JWT_SECRET_AUTH ?? 'change-me-in-production',
+  secret:
+    process.env.JWT_SECRET_AUTH ??
+    'ZEWJ9LtUnTgFPV9BENY264/ivrW4CgaPxcb+qbAi6FIw5LdrBk/BZpqcCTB+Vd+D',
   expiresIn: process.env.JWT_EXPIRES_IN ?? 604800,
 };
