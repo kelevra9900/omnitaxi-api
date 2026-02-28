8206bdfe967b77d56989eba9292bd42cb8b1f1f4
Roger Torres <torresroger445@gmail.com>
2026-02-28 13:12:01 -0600

    under construction flex flow

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,23 +1,11 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
-import { PrismaPg } from '@prisma/adapter-pg';
 import {PrismaClient} from 'generated/prisma/client';
-import { Pool } from 'pg';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
-  constructor() {
-    // 1. Creamos el pool de conexiones de Postgres
-    const pool = new Pool({ 
-      connectionString: process.env.DATABASE_URL 
-    });
-
-    // 2. Creamos el adaptador
-    const adapter = new PrismaPg(pool);
-
-    // 3. Lo pasamos al constructor de PrismaClient
-    super({ adapter });
-  }
-
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
   async onModuleInit() {
     await this.$connect();
   }
